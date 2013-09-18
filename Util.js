3bfc36eb4f2bc5111051e7c7410fc89d98dc47b3
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-09-18 15:19:26 +0200

    Revise assertTypedEqual to actually compare scalars at the leaves.
    Also fixed the code to properly extract the fieldName and type.  The
    code from before seems like it was written as if it were a `for val
    of` loop rather than the actual `for key in` loop that it was using.
    
    Finally, when an assertion failure occurs, this version also prints
    out the original (outermost) inputs prior to the recursive traversal
    of the tree-structure for the input type.

--- Util.js
+++ Util.js
@@ -10,17 +10,33 @@ var int32 = TypedObject.int32;
 var float32 = TypedObject.float32;
 var float64 = TypedObject.float64;
 
-function assertTypedEqual(type, a, b) {
-  if (type instanceof ArrayType) {
-    assertEq(a.length, type.length);
-    assertEq(b.length, type.length);
-    for (var i = 0; i < type.length; i++)
-      assertTypedEqual(type.elementType, a[i], b[i]);
-  } else if (type instanceof StructType) {
-    for (var fieldName in type.fieldNames) {
-      assertTypedEqual(type.fieldTypes[fieldName],
-                       a[fieldName],
-                       b[fieldName]);
-    }
+function assertTypedEqual(type, a_orig, b_orig) {
+  try {
+    recur(type, a_orig, b_orig);
+  } catch (e) {
+    print("failure during "+
+          "assertTypedEqual("+type.toSource()+", "+a_orig.toSource()+", "+b_orig.toSource()+")");
+    throw e;
+  }
+
+  function recur(type, a, b) {
+    if (type instanceof ArrayType) {
+      assertEq(a.length, type.length);
+      assertEq(b.length, type.length);
+      for (var i = 0; i < type.length; i++)
+        recur(type.elementType, a[i], b[i]);
+      } else if (type instanceof StructType) {
+        for (var idx in type.fieldNames) {
+          let fieldName = type.fieldNames[idx];
+          if (type.fieldTypes[fieldName] !== undefined) {
+            recur(type.fieldTypes[fieldName], a[fieldName], b[fieldName]);
+          } else {
+            throw new Error("assertTypedEqual no type for "+
+                            "fieldName: "+fieldName+" in type: "+type.toSource());
+          }
+        }
+      } else {
+        assertEq(a, b);
+      }
   }
 }
