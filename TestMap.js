86ee546ff3085433798a9e83f5347b9fa38c0ce3
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-09-18 11:49:54 +0200

    Filled in the rest of the mapTwoDimensionalArrayOfStructsToStructs test.

--- TestMap.js
+++ TestMap.js
@@ -6,12 +6,16 @@ if (typeof(libdir) === "undefined") { load("Util.js"); } else { load(libdir+"/"+
 // (or its equivalent word in English) and <G1> and <G2> are both
 // grain types (potentially an array themselves.)
 
+var Grain = new StructType({f: uint32});
+function wrapG(v) { return new Grain({f: v}); }
+function doubleG(g) { return new Grain({f: g.f * 2}); }
+function tenG(x, y) { return new Grain({f: x * 10 + y}); }
+
 function mapOneDimensionalArrayOfStructsToStructs() {
-  var grain = new StructType({f: uint32});
-  var type = new ArrayType(grain, 4);
-  var i1 = type.build(x => new grain({f: x}));
-  var r1 = i1.mapPar(type, g => new grain({f: g.f * 2}));
-  var r2 = type.build(x => new grain({f: x * 2}));
+  var type = new ArrayType(Grain, 4);
+  var i1 = type.build(wrapG);
+  var r1 = i1.mapPar(type, doubleG);
+  var r2 = i1.mapPar(type, 1, doubleG);
   assertTypedEqual(type, r1, new type([{f:0}, {f:2},
                                        {f:4}, {f:6}]));
   assertTypedEqual(type, r1, r2);
@@ -20,8 +24,14 @@ function mapOneDimensionalArrayOfStructsToStructs() {
 mapOneDimensionalArrayOfStructsToStructs();
 
 function mapTwoDimensionalArrayOfStructsToStructs() {
-  var grain = new StructType({f: uint32});
-  var type = new ArrayType(new ArrayType(grain, 2), 2);
-  var i1 = type.build(2, (x, y) => { return new grain({f: x * 10 + y}); });
-  // var r1 = i1.mapPar(type, 
+  var rowtype = new ArrayType(Grain, 2);
+  var type = new ArrayType(rowtype, 2);
+  var i1 = type.build(2, tenG);
+  var r1 = i1.mapPar(type, 2, doubleG);
+  var r2 = i1.mapPar(type, 1, (m) => m.mapPar(rowtype, 1, doubleG));
+  assertTypedEqual(type, r1, new type([[{f:00}, {f:01}],
+                                       [{f:10}, {f:11}]]));
+  assertTypedEqual(type, r1, r2);
 }
+
+mapTwoDimensionalArrayOfStructsToStructs();
