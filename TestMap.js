ed0795d2a21fdc50cb276df635106aeaf73d6b64
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-09-18 13:19:24 +0200

    Alpha-rename, "Dim" is less wordy than "Dimensional"

--- TestMap.js
+++ TestMap.js
@@ -2,16 +2,16 @@ if (typeof(libdir) === "undefined") { load("Util.js"); } else { load(libdir+"/"+
 
 // Test name format:
 
-// map<N>DimensionalArrayOf<G1>sTo<G2>s where <N> is a positive integer
-// (or its equivalent word in English) and <G1> and <G2> are both
-// grain types (potentially an array themselves.)
+// map<N>DimArrayOf<G1>sTo<G2>s where <N> is a positive integer (or its
+// equivalent word in English) and <G1> and <G2> are both grain types
+// (potentially an array themselves.)
 
 var Grain = new StructType({f: uint32});
 function wrapG(v) { return new Grain({f: v}); }
 function doubleG(g) { return new Grain({f: g.f * 2}); }
 function tenG(x, y) { return new Grain({f: x * 10 + y}); }
 
-function mapOneDimensionalArrayOfStructsToStructs() {
+function mapOneDimArrayOfStructsToStructs() {
   var type = new ArrayType(Grain, 4);
   var i1 = type.build(wrapG);
   var r1 = i1.mapPar(type, doubleG);
@@ -21,9 +21,9 @@ function mapOneDimensionalArrayOfStructsToStructs() {
   assertTypedEqual(type, r1, r2);
 }
 
-mapOneDimensionalArrayOfStructsToStructs();
+mapOneDimArrayOfStructsToStructs();
 
-function mapTwoDimensionalArrayOfStructsToStructs() {
+function mapTwoDimArrayOfStructsToStructs() {
   var rowtype = new ArrayType(Grain, 2);
   var type = new ArrayType(rowtype, 2);
   var i1 = type.build(2, tenG);
@@ -34,9 +34,9 @@ function mapTwoDimensionalArrayOfStructsToStructs() {
   assertTypedEqual(type, r1, r2);
 }
 
-mapTwoDimensionalArrayOfStructsToStructs();
+mapTwoDimArrayOfStructsToStructs();
 
-function mapOneDimensionalArrayOfStructsToArrayOfStructs() {
+function mapOneDimArrayOfStructsToArrayOfStructs() {
   var Line = new ArrayType(Grain, 2);
   var Box = new ArrayType(Line, 2);
   var i1 = Line.build(wrapG);
@@ -47,4 +47,4 @@ function mapOneDimensionalArrayOfStructsToArrayOfStructs() {
   assertTypedEqual(Box, r1, r2);
 }
 
-mapOneDimensionalArrayOfStructsToArrayOfStructs();
+mapOneDimArrayOfStructsToArrayOfStructs();
