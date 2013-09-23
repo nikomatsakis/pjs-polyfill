1cdfa91b95b5ad4ae2fcf6fee3e1b2b30139468d
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-09-23 17:30:06 +0200

    Updated test code to reflect the corrected map implementation.

--- TestMap.js
+++ TestMap.js
@@ -16,7 +16,7 @@ function mapOneDimArrayOfStructsToStructs() {
   var i1 = type.build(wrapG);
   var r1 = i1.mapPar(type, doubleG);
   var r2 = i1.mapPar(type, 1, doubleG);
-  var r3 = i1.mapPar(type, 1, (g, _, out) => { out.f = g.f * 2; });
+  var r3 = i1.mapPar(type, 1, (g, j, c, out) => { out.f = g.f * 2; });
   assertTypedEqual(type, r1, new type([{f:0}, {f:2},
                                        {f:4}, {f:6}]));
   assertTypedEqual(type, r1, r2);
@@ -29,7 +29,7 @@ function mapTwoDimArrayOfStructsToStructs() {
   var i1 = type.build(2, tenG);
   var r1 = i1.mapPar(type, 2, doubleG);
   var r2 = i1.mapPar(type, 1, (m) => m.mapPar(rowtype, 1, doubleG));
-  var r3 = i1.mapPar(type, 1, (m, _, out) => { out[0].f = m[0].f * 2; out[1].f = m[1].f * 2; });
+  var r3 = i1.mapPar(type, 1, (m, j, c, out) => { out[0].f = m[0].f * 2; out[1].f = m[1].f * 2; });
   assertTypedEqual(type, r1, new type([[{f:00}, {f:02}],
                                        [{f:20}, {f:22}]]));
   assertTypedEqual(type, r1, r2);
@@ -42,7 +42,7 @@ function mapOneDimArrayOfStructsToArrayOfStructs() {
   var i1 = Line.build(wrapG);
   var r1 = i1.mapPar(Box, (g) => Line.build((y) => tenG(g.f, y)));
   var r2 = i1.mapPar(Box, (g) => i1.mapPar(Line, (y) => tenG(g.f, y.f)));
-  var r3 = i1.mapPar(Box, (g, _, out) => { out[0] = tenG(g.f, 0); out[1] = tenG(g.f, 1); });
+  var r3 = i1.mapPar(Box, (g, j, c, out) => { out[0] = tenG(g.f, 0); out[1] = tenG(g.f, 1); });
   assertTypedEqual(Box, r1, new Box([[{f:00}, {f:01}],
                                      [{f:10}, {f:11}]]));
   assertTypedEqual(Box, r1, r2);
